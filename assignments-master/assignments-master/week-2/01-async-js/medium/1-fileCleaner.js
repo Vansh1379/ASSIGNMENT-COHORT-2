13b673203b48c1abee58379d34076897226f75d4
vansh kalra <vanshkalra1379@gmail.com>
2023-12-25 19:30:27 +0530

    New Question

--- assignments-master/assignments-master/week-2/01-async-js/medium/1-fileCleaner.js
+++ assignments-master/assignments-master/week-2/01-async-js/medium/1-fileCleaner.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 function clearFile(){
     let p = new promise(function(resolve){
-        
+        fs.readFile("Demo2.txt", "utf-8",(err,data)=>{
+            if(err) throw err;
+        })
     })
 }
