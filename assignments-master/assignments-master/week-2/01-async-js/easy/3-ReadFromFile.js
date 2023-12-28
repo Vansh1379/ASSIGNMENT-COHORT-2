e21f2470da198cf9485b91613ad48e7d9d63b034
vansh kalra <vanshkalra1379@gmail.com>
2023-12-28 22:27:49 +0530

    Simple

--- assignments-master/assignments-master/week-2/01-async-js/easy/3-ReadFromFile.js
+++ assignments-master/assignments-master/week-2/01-async-js/easy/3-ReadFromFile.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 function ReadFile(){
 
-    let p = new Promise(function(resolve){
+    p = new Promise(function(resolve){
         fs.readFile("Demo.txt", "utf8", function(err,data){
             if(err) throw err;
             resolve(data);
