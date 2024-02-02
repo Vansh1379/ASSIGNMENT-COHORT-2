e36867f9d57cf4dad5e41c428ddff1354b717def
vansh kalra <vanshkalra1379@gmail.com>
2024-02-02 13:44:42 +0530

    ANAGRAM

--- assignments-master/assignments-master/01-js/easy/anagram.js
+++ assignments-master/assignments-master/01-js/easy/anagram.js
@@ -4,10 +4,16 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
-  let str1 = str1.split('').sort().join('');
-  let str2 = str2.split('').sort().join('');
-  return (str1 = str2);
-}
+  function isAnagram(str1, str2) {
+    // Check if lengths of both strings are same
+    let str1 = str1.split('').sort().join('');
+    let str2 = str2.split('').sort().join('');
+    return(str1 = str2);
+  }
+// function isAnagram(str1, str2) {
+//   let str1 = str1.split('').sort().join('');
+//   let str2 = str2.split('').sort().join('');
+//   return (str1 = str2);
+// }
 
 module.exports = isAnagram;
