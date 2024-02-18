1c65846339c68ac68911d069166fb74f040118ae
vansh kalra <vanshkalra1379@gmail.com>
2024-02-18 22:30:52 +0530

    Update

--- assignments-master/assignments-master/week-2/01-async-js/medium/1-fileCleaner.js
+++ assignments-master/assignments-master/week-2/01-async-js/medium/1-fileCleaner.js
@@ -15,11 +15,11 @@
 
 const fs = require('fs');
 
-function clearFile(){
-    let p = new promise(function(resolve){
-        fs.readFile("Demo2.txt", "utf-8",(err,data)=>{
-            if(err) throw err;
+// function clearFile(){
+//     let p = new promise(function(resolve){
+//         fs.readFile("Demo2.txt", "utf-8",(err,data)=>{
+//             if(err) throw err;
             
-        })
-    })
-}
+//         })
+//     })
+// }
