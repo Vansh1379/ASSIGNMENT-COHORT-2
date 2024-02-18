c258efb292d441b2e658624b0c42cba4d9d91a66
vansh kalra <vanshkalra1379@gmail.com>
2024-02-19 01:17:47 +0530

    Week - 2

--- assignments-master/assignments-master/week-2/01-async-js/medium/2-Clock.js
+++ assignments-master/assignments-master/week-2/01-async-js/medium/2-Clock.js
@@ -1,15 +1,34 @@
+  
+
+// function clock(){
+//     let date = new Date();
+//     let hours = date.getHours();
+//     let minute = date.getMinutes();
+//     let second = date.getSeconds();
+    
+//     let ampm = hours >=12 ? "PM" : "AM";
+//     hours =  hours < 10 ? "0" + hours : hours;
+//     minute = minute<10 ? "0" + minute : minute;
+//     second = second<10 ? "0" + second : second;
+    
+//     console.log(`${hours}:${minute}:${second}`);
+// }
+
+// setInterval(clock , 1000);
+
 function clock(){
     let date = new Date();
     let hours = date.getHours();
     let minute = date.getMinutes();
     let second = date.getSeconds();
-    
-    let ampm = hours >=12 ? "PM" : "AM";
-    hours =  hours < 10 ? "0" + hours : hours;
-    minute = minute<10 ? "0" + minute : minute;
-    second = second<10 ? "0" + second : second;
-    
+
+    let ampm = hours>=12 ? "PM" : "AM";
+    hours = hours<10 ? "0"+hours : hours;
+    minute = minute<10 ? "0"+minute : minute ;
+    second = second<10 ? "0" +second :second;
+
     console.log(`${hours}:${minute}:${second}`);
 }
 
-setInterval(clock , 1000);
+
+    setInterval(clock , 1000);
