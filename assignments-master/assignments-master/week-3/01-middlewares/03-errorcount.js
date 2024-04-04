2bfffe7cd67b1aa621636f03b09ece087396d098
vansh kalra <vanshkalra1379@gmail.com>
2024-04-04 16:09:56 +0530

    error count

--- assignments-master/assignments-master/week-3/01-middlewares/03-errorcount.js
+++ assignments-master/assignments-master/week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,8 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use((err, req, res, next)=>{
+  res.status(404).send(err.message);
+  errorCount++;
+})
 module.exports = app;
