45f4a41630c23d85383bb62385dc4226b2c59fcd
vansh kalra <vanshkalra1379@gmail.com>
2024-04-10 22:17:55 +0530

    DB

--- assignments-master/assignments-master/week-3/03-mongo/db/index.js
+++ assignments-master/assignments-master/week-3/03-mongo/db/index.js
@@ -6,6 +6,8 @@ mongoose.connect('your-mongodb-url');
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String
 });
 
 const UserSchema = new mongoose.Schema({
