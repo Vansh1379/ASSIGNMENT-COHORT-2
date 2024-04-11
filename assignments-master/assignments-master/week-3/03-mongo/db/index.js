ae55901365f672225be6e361c7961acbf72b75ec
vansh kalra <vanshkalra1379@gmail.com>
2024-04-11 16:40:21 +0530

    DB

--- assignments-master/assignments-master/week-3/03-mongo/db/index.js
+++ assignments-master/assignments-master/week-3/03-mongo/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://vanshkalra1379:kalra1379@cluster0.rjd2jaw.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
@@ -12,10 +12,20 @@ const AdminSchema = new mongoose.Schema({
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String,
+    purchasedCourses : [{
+        type :mongoose.Schema.Types.ObjectId,
+        ref : 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    tittle : String,
+    description : String, 
+    price : Number,
+    imageLink : String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
