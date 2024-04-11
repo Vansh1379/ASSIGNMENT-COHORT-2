3c15349e88e53ef111c1e7a1c51c598b6e68c940
vansh kalra <vanshkalra1379@gmail.com>
2024-04-11 17:06:38 +0530

    Middleware

--- assignments-master/assignments-master/week-3/03-mongo/middleware/admin.js
+++ assignments-master/assignments-master/week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,22 @@
 // Middleware for handling auth
+const {Admin} = require("../db");
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const username = req.headers.username; // vanshkalra@gmail.com
+    const password = req.headers.password; // 123456
+    Admin.findOne({
+        username : username,
+        password : password
+    }).then((value)=>{
+        if(value){
+            next();
+        } else{
+            res.status(401).json({
+                message : "Admin is Unauthorized"
+            })
+        }
+    })
 }
 
 module.exports = adminMiddleware;
