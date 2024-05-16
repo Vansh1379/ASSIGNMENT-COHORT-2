171f838a62e5f25fde830277731bacac5e7a4d41
vansh kalra <vanshkalra1379@gmail.com>
2024-05-16 17:14:22 +0530

    Week-5

--- assignments-master/assignments-master/week-5/level-1/Bussiness Card/src/components/Heading.jsx
+++ assignments-master/assignments-master/week-5/level-1/Bussiness Card/src/components/Heading.jsx
@@ -2,7 +2,7 @@ import "./Heading.css"
 export function Heading() {
     return (
         <div className="Heading">
-            <h1 className="Lulu">Ldeshwar</h1>
+            <h1 className="Lulu">Lundeshwar</h1>
             <h3 className="BJP">Welcome to BJP</h3>
         </div>
     );
