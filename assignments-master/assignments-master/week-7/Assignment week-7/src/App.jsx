214123fc0ea55c342243aa6f467a945c3d5c3bf0
vansh kalra <vanshkalra1379@gmail.com>
2024-06-07 06:33:49 +0530

    Assignment -1

--- assignments-master/assignments-master/week-7/Assignment week-7/src/App.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { Card } from '../components/Assignment-1/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
