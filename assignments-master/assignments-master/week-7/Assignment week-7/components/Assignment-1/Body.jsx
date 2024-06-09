e9b34b5722b15288d0af90ade0182a9a47cdcbbc
vansh kalra <vanshkalra1379@gmail.com>
2024-06-09 22:53:29 +0530

    A

---
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-1/Body.jsx
@@ -0,0 +1,7 @@
+export function Body() {
+    return (
+        <div>
+
+        </div>
+    );
+}
---
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-1/Fotter.jsx
@@ -0,0 +1,7 @@
+export function Fotter() {
+    return (
+        <div>
+
+        </div>
+    );
+}
