966bf1160a81195da0b452e03946b2172d0ecd93
vansh kalra <vanshkalra1379@gmail.com>
2024-06-13 22:20:20 +0530

    Assignment -2

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-2/BackgroundRender.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-2/BackgroundRender.jsx
@@ -4,9 +4,10 @@ import "./BackgroundRender.css"
 export function BackgroundRender() {
     return (
         <div className="Background">
-            <Button color="red" />
+            <Button color="red" className="red" />
             <Button color="Yellow" />
             <Button color="Balck" />
+            <Button />
             <Button color="Purple" />
             <Button color="green" />
             <Button color="blue" />
