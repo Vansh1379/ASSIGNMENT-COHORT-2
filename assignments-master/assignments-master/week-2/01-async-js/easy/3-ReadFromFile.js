5d7ce05779624ec13180f63b56a56fdfb05b8690
vansh kalra <vanshkalra1379@gmail.com>
2024-06-27 21:13:44 +0530

    Week-2

--- assignments-master/assignments-master/week-2/01-async-js/easy/3-ReadFromFile.js
+++ assignments-master/assignments-master/week-2/01-async-js/easy/3-ReadFromFile.js
@@ -1,23 +1,16 @@
 const fs = require("fs");
 
-function ReadFile(){
-
-    p = new Promise(function(resolve){
-        fs.readFile("Demo.txt", "utf8", function(err,data){
-            if(err) throw err;
+function ReadFile() {
+    p = new Promise(function (resolve) {
+        fs.readFile("Demo.txt", "utf8", function (err, data) {
+            if (err) throw err;
             resolve(data);
         });
     })
     return p;
 }
 
-async function print(){
+async function print() {
     let value = await ReadFile();
     console.log(value);
 }
-
-// function print(data){
-//     console.log(data);
-// }
-
-// ReadFile().then(print);
--- assignments-master/assignments-master/week-2/01-async-js/easy/4-WriteToFile.js
+++ assignments-master/assignments-master/week-2/01-async-js/easy/4-WriteToFile.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-
         fs.writeFile("Demo.txt", "Ab miane mara ye wala", function(err){
             if(err) throw err;
             console.log("File has been saved");
