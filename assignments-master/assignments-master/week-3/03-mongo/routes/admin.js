5a0348f6cdacbfa7397a962dc51dbd02af039610
vansh kalra <vanshkalra1379@gmail.com>
2024-07-09 22:58:24 +0530

    Admin route

--- assignments-master/assignments-master/week-3/03-mongo/routes/admin.js
+++ assignments-master/assignments-master/week-3/03-mongo/routes/admin.js
@@ -6,9 +6,26 @@ const router = express.Router();
 // Admin Routes
 router.post('/signup', async (req, res) => {
     // Implement admin signup logic
-   
+    const username = req.body.username;
+    const password = req.body.password;
+
     // now we will check if the user exists in the database
-    
+
+    Admin.Create({
+        username,
+        password
+    })
+    .then(admin)=>{
+        res.json({
+            msg: "Admin created succesfully"
+        })
+        next();
+    }
+    .catch(err)=>{
+        res.json({
+            msg:"Admin not created"
+        })
+    }
 });
 
 router.post('/courses', adminMiddleware, async (req, res) => {
