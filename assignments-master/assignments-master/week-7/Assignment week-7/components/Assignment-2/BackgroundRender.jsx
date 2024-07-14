accee9702064f2e7ae58647901de056187c9cc6b
vansh kalra <vanshkalra1379@gmail.com>
2024-07-15 00:45:12 +0530

    Assignment -2

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-2/BackgroundRender.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-2/BackgroundRender.jsx
@@ -1,15 +1,33 @@
 import { Button } from "./Button";
 import "./BackgroundRender.css"
 
+import { useState } from "react"
 export function BackgroundRender() {
+    const [background, setBackground] = useState([{
+        id: 1,
+        color: "red"
+    }, {
+        id: 2,
+        color: "blue"
+    }, {
+        id: 3,
+        color: "green"
+    }, {
+        id: 4,
+        color: "yellow"
+    }, {
+        id: 5,
+        color: "black"
+    }, {
+        id: 6,
+        color: "white"
+    }, {
+        id: 7,
+        color: "pink"
+    }])
     return (
         <div className="Background">
-            <Button color="red" className="red" />
-            <Button color="Yellow" />
-            <Button color="Balck" />
-            <Button color="Purple" />
-            <Button color="green" />
-            <Button color="blue" />
+            {background.map((background) => (<button key={background.id} color={background.color} />))}
             <Button color="default" />
         </div>
     );
