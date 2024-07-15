9402460a8761126466ab92e84b36eb4db28a3649
vansh kalra <vanshkalra1379@gmail.com>
2024-07-16 00:53:29 +0530

    ParaGenerator

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-4/ParaGenerator.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-4/ParaGenerator.jsx
@@ -4,7 +4,9 @@ export function ParaGenerator() {
         <div className="movediv">
             <h1>Para Generator</h1>
             <input type="text" name="" placeholder="Enter No of Words You want bitch" id="inputBox" />
-            <button className="buten">Generate</button>
+            <button className="buten" onChange={function(e){
+                
+            }}>Generate</button>
         </div>
     );
 }
