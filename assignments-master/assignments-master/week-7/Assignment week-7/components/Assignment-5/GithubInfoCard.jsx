08ec9cda1fc8f3547e071463b5c9c91e17d2cbbe
vansh kalra <vanshkalra1379@gmail.com>
2024-07-16 22:04:43 +0530

    GithubInfoCard

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-5/GithubInfoCard.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-5/GithubInfoCard.jsx
@@ -9,26 +9,37 @@ export function GithubInfoCard() {
     const [Userid, setUserid] = useState();
     const [UserCompany, setUserCompany] = useState();
     const [Userrepo, setuserrepo] = useState();
+    const [UserTwitter, setUserTwitter] = useState();
+    const [UserFollowers, setUserFollowers] = useState();
+    const [UserFollowing, setUserFollowing] = useState();
 
     useEffect(() => {
         axios.get("https://api.github.com/users/Vansh1379")
             .then((response) => {
+                console.log(response.data);
                 setUsername(response.data.name);
                 setUser(response.data.login);
                 setUserid(response.data.id);
                 setUserCompany(response.data.company);
                 setuserrepo(response.data.repos_url);
+                setUserTwitter(response.data.twitter_username);
+                setUserFollowers(response.data.followers);
+                setUserFollowing(response.data.following);
             })
     }, []);
 
     return (
         <div className="InfoCard">
             <h1 className="heading">Github Info Card</h1>
-            <h3>Username :- {User}</h3>
-            <h3>Nmae :- {Username}</h3>
-            <h3>User Id :- {Userid}</h3>
-            <h3>Company :- {UserCompany}</h3>
-            <h3>Repos :- {Userrepo}</h3>
+            <input type="text" name="" placeholder="Enter Github Username" id="" />
+            <h3 className="info">Username :- {User}</h3>
+            <h3 className="info">Nmae :- {Username}</h3>
+            <h3 className="info">User Id :- {Userid}</h3>
+            <h3 className="info">Company :- {UserCompany}</h3>
+            <h3 className="info">Repos :- {Userrepo}</h3>
+            <h3 className="info">Twitter :- {UserTwitter}</h3>
+            <h3 className="info">Followers :- {UserFollowers}</h3>
+            <h3 className="info">Following :- {UserFollowing}</h3>
         </div>
     );
 }
