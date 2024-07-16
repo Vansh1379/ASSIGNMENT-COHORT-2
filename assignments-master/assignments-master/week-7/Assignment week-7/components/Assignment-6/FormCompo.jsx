fa7b30f7d6084665b9022701d11703ad086826ac
vansh kalra <vanshkalra1379@gmail.com>
2024-07-17 01:11:26 +0530

    FronCompo

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-6/FormCompo.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-6/FormCompo.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react"
 import "./FormCompo.css"
 
 export function FromComponent() {
+    const [phoneNumber, setPhoneNumber] = useState("");
+
+    function handlePhoneNumber() {
+
+    }
+
+    function handlePhoneSubmit() {
+
+    }
+
     return (
         <div>
-
+            <form onSubmit={handlePhoneSubmit}>
+                <input
+                    type="text"
+                    onChange={handlePhoneNumber}
+                    value={phoneNumber}
+                    placeholder="Enter your phone number"
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
