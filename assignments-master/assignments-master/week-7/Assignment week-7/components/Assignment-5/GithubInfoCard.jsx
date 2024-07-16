eaa1d582b0d4c3e018e11c0915959922aa3771e4
vansh kalra <vanshkalra1379@gmail.com>
2024-07-16 21:56:08 +0530

    GithubInfoCard

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-5/GithubInfoCard.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-5/GithubInfoCard.jsx
@@ -7,12 +7,17 @@ export function GithubInfoCard() {
     const [Username, setUsername] = useState();
     const [User, setUser] = useState();
     const [Userid, setUserid] = useState();
+    const [UserCompany, setUserCompany] = useState();
+    const [Userrepo, setuserrepo] = useState();
 
     useEffect(() => {
         axios.get("https://api.github.com/users/Vansh1379")
             .then((response) => {
+                setUsername(response.data.name);
                 setUser(response.data.login);
                 setUserid(response.data.id);
+                setUserCompany(response.data.company);
+                setuserrepo(response.data.repos_url);
             })
     }, []);
 
@@ -20,7 +25,10 @@ export function GithubInfoCard() {
         <div className="InfoCard">
             <h1 className="heading">Github Info Card</h1>
             <h3>Username :- {User}</h3>
+            <h3>Nmae :- {Username}</h3>
             <h3>User Id :- {Userid}</h3>
+            <h3>Company :- {UserCompany}</h3>
+            <h3>Repos :- {Userrepo}</h3>
         </div>
     );
 }
