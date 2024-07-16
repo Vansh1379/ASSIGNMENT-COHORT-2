6e230417058ead48fcb7549c36c801df993f65f5
vansh kalra <vanshkalra1379@gmail.com>
2024-07-16 15:06:26 +0530

    GithubInfoCard

--- assignments-master/assignments-master/week-7/Assignment week-7/src/App.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/src/App.jsx
@@ -2,11 +2,12 @@ import './App.css'
 import { Card } from '../components/Assignment-1/Card'
 import { BackgroundRender } from '../components/Assignment-2/BackgroundRender'
 import { ParaGenerator } from '../components/Assignment-4/ParaGenerator'
+import { GithubInfoCard } from '../components/Assignment-5/GithubInfoCard';
 
 function App() {
   return (
-    <div>
-      <ParaGenerator />
+    <div className='github'>
+      <GithubInfoCard />
     </div>
   );
 }
