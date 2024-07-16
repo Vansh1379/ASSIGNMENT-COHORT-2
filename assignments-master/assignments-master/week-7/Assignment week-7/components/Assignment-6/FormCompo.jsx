d3f1cbb496a2cfe500c204c62907510775d4b7ff
vansh kalra <vanshkalra1379@gmail.com>
2024-07-17 01:19:57 +0530

    FronCompo

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-6/FormCompo.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-6/FormCompo.jsx
@@ -3,14 +3,23 @@ import "./FormCompo.css"
 
 export function FromComponent() {
     const [phoneNumber, setPhoneNumber] = useState("");
+    const [showOtpInput, setShowOtpInput] = useState(false);
 
-    function handlePhoneNumber() {
+    function handlePhoneNumber(e) {
+        setPhoneNumber(e.target.value);
+    };
 
-    }
+    function handlePhoneSubmit(event) {
+        event.preventDefault();
 
-    function handlePhoneSubmit() {
-
-    }
+        // we will use regex for check the phine number is valid or not 
+        const regex = /[^0-9]/g;
+        if (phoneNumber.length < 10 || regex.test(phoneNumber)) {
+            alert("Fucking Invalid Phone Number Bruh!");
+            return;
+        }
+        setShowOtpInput(true);
+    };
 
     return (
         <div>
