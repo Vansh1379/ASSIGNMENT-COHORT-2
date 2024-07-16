b68457c5b5effc6a90a2fc1762aafa7a8ba7a3e9
vansh kalra <vanshkalra1379@gmail.com>
2024-07-16 21:48:46 +0530

    GithubInfoCard

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-5/GithubInfoCard.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-5/GithubInfoCard.jsx
@@ -5,19 +5,22 @@ import "./GithubInfoCard.css"
 export function GithubInfoCard() {
 
     const [Username, setUsername] = useState();
-    
+    const [User, setUser] = useState();
+    const [Userid, setUserid] = useState();
+
     useEffect(() => {
         axios.get("https://api.github.com/users/Vansh1379")
             .then((response) => {
-                console.log(response.data);
-                console.log(response.data.login);
-                console.log(response.data.id);
+                setUser(response.data.login);
+                setUserid(response.data.id);
             })
     }, []);
 
     return (
         <div className="InfoCard">
             <h1 className="heading">Github Info Card</h1>
+            <h3>Username :- {User}</h3>
+            <h3>User Id :- {Userid}</h3>
         </div>
     );
 }
