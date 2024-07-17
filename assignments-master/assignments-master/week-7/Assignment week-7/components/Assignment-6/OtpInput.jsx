11a18715043909af4fe5e811ef9dda9de1fd57af
vansh kalra <vanshkalra1379@gmail.com>
2024-07-18 02:28:31 +0530

    Assignment-7

--- assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-6/OtpInput.jsx
+++ assignments-master/assignments-master/week-7/Assignment week-7/components/Assignment-6/OtpInput.jsx
@@ -5,9 +5,29 @@ export function OtpInput({ length, onOtpSubmit }) {
     const [otp, setOtp] = useState(new Array(length).fill(""));
     console.log(otp);
 
+    function handleChange() {
+
+    }
+
+    function handleClick() {
+
+    }
+
+    function handleKeyDown() {
+
+    }
+
     return (
         <div>
-
+            {
+                Otp.map((value, index) => {
+                    return <input key={index} type="text" value={value}
+                        onChange={() => handleChange(index, e)}
+                        onClick={() => handleClick(index)}
+                        onKeyDown={() => handleKeyDown()}
+                    />
+                })
+            }
         </div>
     )
 }
